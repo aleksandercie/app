443223334e6ffd83d75111ca47198e300b58f822
Aleksander Gasior <aleksander.cieszyn@gmail.com>
2019-03-11 11:00:32 +0100

    Updates

---
+++ precache-manifest.68d94e0e3a9c104833dd8634f08fd29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a09771fbcc381c27be93603337566184",
+    "url": "/app/static/media/image.a09771fb.jpg"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "71729dee8e394ebac8a1",
+    "url": "/app/static/js/main.a9bde463.chunk.js"
+  },
+  {
+    "revision": "976d47a9f90ec84d1fae",
+    "url": "/app/static/js/2.c8e6c8a0.chunk.js"
+  },
+  {
+    "revision": "71729dee8e394ebac8a1",
+    "url": "/app/static/css/main.1ddcd38a.chunk.css"
+  },
+  {
+    "revision": "105004e95233e494f82d933784ae3eb2",
+    "url": "/app/index.html"
+  }
+];
